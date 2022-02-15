842fadda08762d1d59b56413786d897ff93bc0e1
hernandez87v <hernandez87v@gmail.com>
2022-02-15 11:56:29 -0800

    add menu button and appBar title

--- lib/home.dart
+++ lib/home.dart
@@ -23,8 +23,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('METRO'),
+        // TODO: Add trailing buttons (102)
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
