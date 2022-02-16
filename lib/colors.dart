6b16830f56f6249856bcdfa2a103c1857c1f61db
hernandez87v <hernandez87v@gmail.com>
2022-02-16 10:45:30 -0800

    align colors accross app

--- lib/colors.dart
+++ lib/colors.dart
@@ -14,12 +14,12 @@
 
 import 'package:flutter/material.dart';
 
-const kShrineRed50 = Color(0xFFf98b00);
+const kShrineRed50 = Color(0xFFF2AC57);
 const kShrineRed100 = Color(0xFF1a237e);
 const kShrineRed300 = Color(0xFF534bae);
 const kShrineRed400 = Color(0xFFC02124);
 
-const kShrineBrown900 = Color(0xFFc05d00);
+const kShrineBrown900 = Color(0xFFF2AC57);
 
 const kShrineErrorRed = Color(0xFFC5032B);
 
