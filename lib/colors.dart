52b6606b949824e7f28685cafd31b4d20ddf7978
hernandez87v <hernandez87v@gmail.com>
2022-02-15 16:37:59 -0800

    add custom color pallette for theme

---
+++ lib/colors.dart
@@ -0,0 +1,27 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+const kShrineRed50 = Color(0xFFf98b00);
+const kShrineRed100 = Color(0xFF1a237e);
+const kShrineRed300 = Color(0xFF534bae);
+const kShrineRed400 = Color(0xFFC02124);
+
+const kShrineBrown900 = Color(0xFFc05d00);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
