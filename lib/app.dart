6d9eb94ca090be6c2bba5aaad32935ef65ef0b75
hernandez87v <hernandez87v@gmail.com>
2022-02-17 13:34:07 -0800

    add CutCornersBorder decoration to username and password

--- lib/app.dart
+++ lib/app.dart
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'supplemental/cut_corners_border.dart';
 
 import 'home.dart';
 import 'login.dart';
@@ -71,13 +72,13 @@ ThemeData _buildShrineTheme() {
     // TODO: Add the icon themes (103)
     // TODO: Decorate the inputs (103)
     inputDecorationTheme: const InputDecorationTheme(
-      focusedBorder: OutlineInputBorder(
+      focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
           color: kShrineBrown900,
         ),
       ),
-      border: OutlineInputBorder(),
+      border: CutCornersBorder(),
     ),
   );
 }
