6063e3c22eb82e156c8accc629f31df2d08e12ab
hernandez87v <hernandez87v@gmail.com>
2022-02-22 11:52:33 -0800

    add backdrop state

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: todo
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
@@ -33,7 +34,6 @@ class Backdrop extends StatefulWidget {
 class _BackdropState extends State<Backdrop>
     with SingleTickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
-
   // TODO: Add AnimationController widget (104)
 
   // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
@@ -51,7 +51,7 @@ class _BackdropState extends State<Backdrop>
   @override
   Widget build(BuildContext context) {
     var appBar = AppBar(
-      brightness: Brightness.light,
+      // brightness: Brightness.light,
       elevation: 0.0,
       titleSpacing: 0.0,
       // TODO: Replace leading menu icon with IconButton (104)
